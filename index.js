894b5e59a1e2dd4bb121b759e1bb5dbbf40f0bc2
kesatriapoibear-ui <kesatriapoibear@gmail.com>
2025-10-08 10:40:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from "express"
 import panelRoutes from "./api/panel.js"
+import aiRoutes from "./api/ai.js"
 
 const app = express()
 app.use(express.json())
 
-for (const route of panelRoutes) {
+// Gabungkan semua route
+const routes = [...panelRoutes, ...aiRoutes]
+
+for (const route of routes) {
   app[route.method.toLowerCase()](route.path, route.handler)
 }
+
+// Endpoint root
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+    status: "online",
+    endpoints: routes.map(r => ({ method: r.method, path: r.path })),
+  })
+})
 
 export default app
