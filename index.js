c5868744b85e56065115af4d2f25af7179f4784a
kesatriapoibear-ui <kesatriapoibear@gmail.com>
2025-10-08 16:09:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// index.js
 import express from "express"
 import panelRoutes from "./api/panel.js"
 
@@ -5,7 +6,7 @@ const app = express()
 app.use(express.json())
 
 for (const route of panelRoutes) {
-  app[route.method.toLowerCase()](route.path, route.handler)
+  app[route.method.toLowerCase()](`/api/panel${route.path}`, route.handler)
 }
 
 export default app
